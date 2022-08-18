8d4f5fcf94da0649c02f84cbebf45788ffacef34
Marcelo Cisneros <mc9cisneros@gmail.com>
2022-08-17 21:40:16 -0500

    Mmm mmm

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'Hola'; "Chelita"
 const nuevoNum = "2704";
 
 // Crea una variable booleana:
-const nuevoBool = "false";
+const nuevoBool = "true";
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = "10 - 5" === 5;
